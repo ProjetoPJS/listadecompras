0e587c62017fc79873af70c647ca103ef090883b
unknown <IFSP@PC08LAB3.(none)>
2012-08-14 19:23:16 -0300

    Nao Alterei Nada

--- ListadeCompras/Form1.Designer.cs
+++ ListadeCompras/Form1.Designer.cs
@@ -71,7 +71,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(324, 316);
+            this.button2.Location = new System.Drawing.Point(324, 278);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
